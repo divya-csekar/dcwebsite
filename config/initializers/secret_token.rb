388c5092336f9f0b1211196a78617ce74d3a0d33
Divya Chandra Sekar <divya.chandra.sekar@sv.cmu.edu>
2013-09-13 22:29:53 -0400

    Initialize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dcwebsite::Application.config.secret_token = 'a8ad276d245016a852c2f671f0b1c46c106f4d8432e2dda415b8842045bd93530f1eb5f03f627e70a34b81d011e41187155d58dc59feb0473050aaa1390ac88e'
