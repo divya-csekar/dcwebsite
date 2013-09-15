1de10117705023794e1bc7ed724426c7ca7afd26
Divya Chandra Sekar <divya.chandra.sekar@sv.cmu.edu>
2013-09-15 01:31:34 -0400

    AJAX

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,13 @@ Dcwebsite::Application.routes.draw do
 
   resources :albums
 
-  get "static_pages/home"
+  get 'static_pages/home' , to: 'static_pages#home', as: 'static_home_path'
 
-  get "static_pages/help"
+  get 'static_pages/help' , to: 'static_pages#help', as: 'static_help_path'
 
-  get "static_pages/main"
+  get 'static_pages/main' , to: 'static_pages#main', as: 'static_main_path'
 
-  get "static_pages/about"
+  get 'static_pages/about' , to: 'static_pages#about', as: 'static_about_path'
 
   match '/musicstore',    :to => 'music_store#index', via: 'get'
 
